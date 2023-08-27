87b094e8dd0858983c105d5a6b01b18d956e40ec
feeltrig <krunalvaghadia@gmail.com>
2023-08-27 12:59:05 +0530

    login auth flow

--- pages/authpages/login.jsx
+++ pages/authpages/login.jsx
@@ -2,9 +2,14 @@ import {Box, Button, Center, Input, Text, VStack} from "@chakra-ui/react";
 import React, {useState} from "react";
 import {getCsrfToken} from "next-auth/react";
 import {signIn} from "next-auth/react";
+import {toggleObjectState} from "../../Funtions/dataFunctions";
 
 const login = ({csrfToken}) => {
-  const asd = () => {
+  const [creds, setcreds] = useState({
+    username: "",
+    password: "",
+  });
+  const authenticate = () => {
     signIn("credentials", {
       username: "jsmith",
       password: "1234",
@@ -15,24 +20,19 @@ const login = ({csrfToken}) => {
     <Box height={"100%"}>
       <Center minH={"100%"}>
         <VStack bg="white" p={"2rem"} borderRadius="2xl">
-          <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-          <form
-            method="post"
-            onSubmit={(e) => {
-              e.preventDefault();
-              asd();
-            }}
-          >
-            <label>
-              Username
-              <input name="username" type="text" />
-            </label>
-            <label>
-              Password
-              <input name="password" type="password" />
-            </label>
-            <button type="submit">Sign in</button>
-          </form>
+          <Text>Username</Text>
+          <Input
+            onChange={(e) =>
+              toggleObjectState(setcreds, "username", e.target.value)
+            }
+          />
+          <Text>Password</Text>
+          <Input
+            onChange={(e) =>
+              toggleObjectState(setcreds, "password", e.target.value)
+            }
+          />
+          <Button onClick={authenticate}>Login</Button>
         </VStack>
       </Center>
     </Box>
